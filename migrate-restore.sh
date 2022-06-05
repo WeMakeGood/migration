3de90a04c8e0c26e5ebd28257a5135fa3c2b237d
Christopher Frazier <chris.frazier@managedword.com>
2022-06-05 09:47:23 -0700

    Fixing header.

--- migrate-restore.sh
+++ migrate-restore.sh
@@ -20,11 +20,12 @@ SERVERNAME="$(hostname)"
 API_URL="https://forge.laravel.com/api/v1"
 
 # Set up some Forge information
-HEADERS=(-H "Authorization: Bearer '"$API_TOKEN"'" -H "Accept: application/json" -H "Content-Type: application/json")
+HEADERS=(-H "Authorization: Bearer $API_TOKEN" -H "Accept: application/json" -H "Content-Type: application/json")
 SERVER_ID="$(curl -s "${HEADERS[@]}" -X GET $API_URL/servers -s | jq '.servers[] | select(.name="$SERVERNAME").id')"
 SERVER_SITES="$(curl "${HEADERS[@]}" -X GET $API_URL/servers/$SERVER_ID/sites -s | jq -r '.sites[].name')"
 DB_USER_ID="$(curl "${HEADERS[@]}" -X GET $API_URL/servers/$SERVER_ID/database-users -s | jq '.users[] | select(.name=='\"$USER\"').id')"
 
+echo "$API_TOKEN"
 echo "$(curl "${HEADERS[@]}" -X GET $API_URL/servers)"
 
 echo "System Configuration"
