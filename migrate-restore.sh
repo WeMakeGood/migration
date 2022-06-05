6ef6d3af3a585ee6188b751ee44e1c1bc3ce019c
Christopher Frazier <chris.frazier@managedword.com>
2022-06-05 09:56:08 -0700

    Test

--- migrate-restore.sh
+++ migrate-restore.sh
@@ -19,19 +19,14 @@ SITES="$MIGRATE/sites.csv"
 SERVERNAME="$(hostname)"
 API_URL="https://forge.laravel.com/api/v1"
 
-# Set up CURL
-curl -i -H "Authorization: Bearer $API_TOKEN" \
-	-H "Accept: application/json" \
-	-H "Content-Type: application/json"
-
 # Set up some Forge information
 HEADERS=(-H "Authorization: Bearer $API_TOKEN" -H "Accept: application/json" -H "Content-Type: application/json")
-SERVER_ID="$(curl -X GET $API_URL/servers -s | jq '.servers[] | select(.name="$SERVERNAME").id')"
-SERVER_SITES="$(curl -X GET $API_URL/servers/$SERVER_ID/sites -s | jq -r '.sites[].name')"
-DB_USER_ID="$(curl -X GET $API_URL/servers/$SERVER_ID/database-users -s | jq '.users[] | select(.name=='\"$USER\"').id')"
-
 echo "$API_TOKEN"
-echo "$(curl -X GET $API_URL/servers)"
+echo "$(curl "${HEADERS[@]}" -X GET $API_URL/servers)"
+
+SERVER_ID="$(curl -s "${HEADERS[@]}" -X GET $API_URL/servers -s | jq '.servers[] | select(.name="$SERVERNAME").id')"
+SERVER_SITES="$(curl "${HEADERS[@]}" -X GET $API_URL/servers/$SERVER_ID/sites -s | jq -r '.sites[].name')"
+DB_USER_ID="$(curl "${HEADERS[@]}" -X GET $API_URL/servers/$SERVER_ID/database-users -s | jq '.users[] | select(.name=='\"$USER\"').id')"
 
 echo "System Configuration"
 echo "  Server Name: $SERVERNAME"
@@ -75,12 +70,12 @@ while read APP SITE URL ARCHIVE PREFIX NEWSITE; do
 	SITE_ROOT="$ROOT/$SITE/public"
 
 	# Check for an existing database
-	SITE_DB_ID="$(curl -X GET $API_URL/servers/$SERVER_ID/databases | jq '.databases[] | select(.name=="'$SITE_DB_NAME'").id')"
+	SITE_DB_ID="$(curl -s "${HEADERS[@]}" -X GET $API_URL/servers/$SERVER_ID/databases | jq '.databases[] | select(.name=="'$SITE_DB_NAME'").id')"
 	# Create a new database if it needs to be added
 	if [ -v $SITE_DB_ID ]; then
 		echo "Adding database..."
 		# Create the database
-		SITE_DB_ID="$(curl -X POST $API_URL/servers/$SERVER_ID/databases --data '{"name":"'"$SITE_DB_NAME"'"}' | jq '.database.id')"
+		SITE_DB_ID="$(curl -s "${HEADERS[@]}" -X POST $API_URL/servers/$SERVER_ID/databases --data '{"name":"'"$SITE_DB_NAME"'"}' | jq '.database.id')"
 		if [[ ! "$SITE_DB_ID" =~ ^-?[0-9]+$ ]]; then
 			echo $SITE_DB_ID
 			exit 99
@@ -88,17 +83,17 @@ while read APP SITE URL ARCHIVE PREFIX NEWSITE; do
 	fi
 
 	# Add the database to the user
-	DB_USER_DBS="$(curl -X GET $API_URL/servers/$SERVER_ID/database-users/$DB_USER_ID | jq -c '.user.databases')"
+	DB_USER_DBS="$(curl -s "${HEADERS[@]}" -X GET $API_URL/servers/$SERVER_ID/database-users/$DB_USER_ID | jq -c '.user.databases')"
 	if [[ ! "$DB_USER_DBS" == *"$SITE_DB_ID"* ]]; then
 		DB_USER_DBS="$(echo "$DB_USER_DBS" | jq -c '. += ['$SITE_DB_ID'] | unique')"
-		curl -X PUT $API_URL/servers/$SERVER_ID/database-users/$DB_USER_ID -d '{"databases": '"$DB_USER_DBS"'}' >/dev/null
+		curl -s "${HEADERS[@]}" -X PUT $API_URL/servers/$SERVER_ID/database-users/$DB_USER_ID -d '{"databases": '"$DB_USER_DBS"'}' >/dev/null
 	fi
 
 	# Get the site ID or make it
-	SITE_ID="$(curl -X GET $API_URL/servers/$SERVER_ID/sites | jq '.sites[] | select(.name=="'$SITE'").id')"
+	SITE_ID="$(curl -s "${HEADERS[@]}" -X GET $API_URL/servers/$SERVER_ID/sites | jq '.sites[] | select(.name=="'$SITE'").id')"
 	if [ -v $SITE_ID ]; then
 		echo "Creating site..."
-		SITE_ID="$(curl -X POST $API_URL/servers/$SERVER_ID/sites -d '{"domain":"'"$SITE"'","project_type":"php","directory":"/public","isolated":true,"username":"'"$USER"'","php_version":"php80"}' | jq -cr '.site.id')"
+		SITE_ID="$(curl -s "${HEADERS[@]}" -X POST $API_URL/servers/$SERVER_ID/sites -d '{"domain":"'"$SITE"'","project_type":"php","directory":"/public","isolated":true,"username":"'"$USER"'","php_version":"php80"}' | jq -cr '.site.id')"
 		if [[ ! "$SITE_ID" =~ ^-?[0-9]+$ ]]; then
 			echo $SITE_ID
 			exit 99
@@ -108,7 +103,7 @@ while read APP SITE URL ARCHIVE PREFIX NEWSITE; do
 	# Install WordPress if we need it
 	if [ ! -f $SITE_ROOT/wp-config.php ]; then
 		echo "Installing WordPress..."
-		curl -X POST $API_URL/servers/$SERVER_ID/sites/$SITE_ID/wordpress -d '{"database": "'"$SITE_DB_NAME"'", "user": '"$DB_USER_ID"'}' >/dev/null
+		curl -s "${HEADERS[@]}" -X POST $API_URL/servers/$SERVER_ID/sites/$SITE_ID/wordpress -d '{"database": "'"$SITE_DB_NAME"'", "user": '"$DB_USER_ID"'}' >/dev/null
 		until [ -f $SITE_ROOT/wp-config.php ]; do
 			sleep 1
 		done
