bdeb151feb2339941b683fa7866b1056c5e3af62
Christopher Frazier <chris.frazier@managedword.com>
2022-06-07 12:31:52 -0700

    Test

--- migrate-restore.sh
+++ migrate-restore.sh
@@ -95,7 +95,7 @@ while read APP SITE URL ARCHIVE PREFIX NEWSITE; do
 	else
 		# Create a new DB user if providing shared hosting
 		DB_PASSWORD="$(tr -cd '[:alnum:]' </dev/urandom | fold -w30 | head -n1)"
-		SITE_DB_USER="$(curl --trace-ascii "${HEADERS[@]}" -X POST $API_URL/servers/$SERVER_ID/database-users -d '{"name":"'$SITE_DB_NAME'","password":"'$DB_PASSWORD'","databases": ['"$SITE_DB_ID"']}' | jq -cr '.')"
+		SITE_DB_USER="$(curl -s "${HEADERS[@]}" -X POST $API_URL/servers/$SERVER_ID/database-users -d '{"name":"'$SITE_DB_NAME'","password":"'$DB_PASSWORD'","databases": ['"$SITE_DB_ID"']}' | jq -cr '.')"
 		echo "$SITE_DB_USER"
 		exit 99
 	fi
