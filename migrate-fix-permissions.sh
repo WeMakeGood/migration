a7ea040d605cf8b279ae1b5d32e8781ecf211a13
Christopher Frazier <chris.frazier@managedword.com>
2022-06-08 14:03:04 -0700

    Fixed escaping.

--- migrate-fix-permissions.sh
+++ migrate-fix-permissions.sh
@@ -5,10 +5,10 @@ echo "Repairing folder permissions..."
 cd
 ROOT="$(pwd)"
 
-for SITE in "$(ls $ROOT)"; do
-	WP_ROOT="$ROOT/$SITE/public"
-	if [ -d "$ROOT/$SITE/public"]; then
+for SITE in $(ls $ROOT); do
+	if [ -d $ROOT/$SITE/public ]; then
 		echo "Repairing $SITE..."
+		WP_ROOT=$ROOT/$SITE/public/wp-content/uploads
 		find ${WP_ROOT} -type d -exec chmod 755 {} \;
 		find ${WP_ROOT} -type f -exec chmod 644 {} \;
 	fi
